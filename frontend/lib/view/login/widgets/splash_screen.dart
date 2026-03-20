84d9537fe358f8527c6444a2a7681946d23812d1
david56w <david.martinez.alp@cbtis258.edu.mx>
2026-03-20 15:20:29 -0600

    carpetas

--- frontend/lib/view/login/widgets/splash_screen.dart
+++ frontend/lib/view/login/widgets/splash_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'dart:async';
-import 'login_page.dart';
+import '../login_page.dart';
 
 class SplashScreen extends StatefulWidget{
   const SplashScreen({super.key});
