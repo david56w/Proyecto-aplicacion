8963032c810c7ef4707fc02ce262075b40490bac
Alfonso243 <gustavo.martinez.alp@cbtis258.edu.mx>
2026-04-10 23:11:59 -0600

    botonregister

--- frontend/lib/view/login/login_page.dart
+++ frontend/lib/view/login/login_page.dart
@@ -58,46 +58,76 @@ class _LoginPageState extends State<LoginPage> {
                     ),
                   ],
                 ),
-              child: SizedBox(
-                width: 400,
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    const Text(
-                      "Bienvenido",
-                      style: TextStyle(
-                        fontSize: 24,
-                        fontWeight: FontWeight.bold,
+                child: SizedBox(
+                  width: 400,
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      const Text(
+                        "Bienvenido",
+                        style: TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                        ),
                       ),
-                    ),
-              const SizedBox(height: 30),
-                  TextField(
-                  controller: emailController,
-                  decoration: InputDecoration(
-                    labelText: "Correo Electronico",
-                      border: OutlineInputBorder(
-                         borderRadius: BorderRadius.circular(12),
+                      const SizedBox(height: 30),
+                      TextField(
+                        controller: emailController,
+                        decoration: InputDecoration(
+                          labelText: "Correo Electronico",
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(12),
+                          ),
+                        ),
                       ),
-                    ),
-                  ),
-                const SizedBox(height: 20),
-                    TextField(
-                    controller: passwordController,
-                    decoration: InputDecoration(
-                      labelText: "Contraseña",
-                        border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(12),
+                      const SizedBox(height: 20),
+                      TextField(
+                        controller: passwordController,
+                        decoration: InputDecoration(
+                          labelText: "Contraseña",
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(12),
+                          ),
+                        ),
+                        obscureText: true,
                       ),
-                    ),
-                      obscureText: true,
-                    ),
-                const SizedBox(height: 30),
-                  Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: <Widget>[
-                      ElevatedButton(
-                        onPressed: () => enviarDatos('login'),
-                        child: Text("Iniciar Sesion"),
+                      const SizedBox(height: 30),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: <Widget>[
+                          ElevatedButton(
+                            onPressed: () => enviarDatos('login'),
+                            child: Text("Iniciar Sesion"),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(height: 30),
+                      Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          const Text(
+                            "¿No tienes cuenta?",
+                            style: TextStyle(
+                              color: Colors.black54,
+                              fontSize: 14,
+                            ),
+                          ),
+                          const SizedBox(height: 5),
+                          TextButton(
+                            onPressed: () {
+                              Navigator.pushNamed(
+                                context,
+                                '/register_page.dart',
+                              );
+                            },
+                            child: const Text("Registrate",
+                                style: TextStyle(
+                                  color: Colors.blue,
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 16,
+                                  decoration: TextDecoration.underline,
+                                ),
+                            ),
                           ),
                         ],
                       ),
---
+++ frontend/lib/view/login/register_page.dart
@@ -0,0 +1,114 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+import 'widgets/custom_header.dart';
+
+class RegisterPage extends StatefulWidget {
+  const RegisterPage({super.key});
+
+  @override
+  State<RegisterPage> createState() => _RegisterPageState();
+}
+
+class _RegisterPageState extends State<RegisterPage> {
+  final formGlobalKey = GlobalKey<FormState>();
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+
+  Future<void> enviarDatos(String ruta) async {
+    final url = Uri.parse('http://10.0.2.2:3000/$ruta');
+
+    final respuesta = await http.post(
+      url,
+      headers: {'Content-Type': 'application/json'},
+      body: jsonEncode({
+        'email': emailController.text,
+        'password': passwordController.text,
+      }),
+    );
+
+    final data = jsonDecode(respuesta.body);
+    if (!mounted) return;
+    ScaffoldMessenger.of(
+      context,
+    ).showSnackBar(SnackBar(content: Text(data['mensaje'])));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 252, 252, 252),
+      body: Column(
+        children: [
+          CustomHeader(),
+          Expanded(
+            child: SingleChildScrollView(
+              padding: const EdgeInsets.all(150.0),
+              child: Container(
+                padding: const EdgeInsets.all(30.0),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(20),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.black12,
+                      blurRadius: 15,
+                      offset: Offset(0, 10),
+                    ),
+                  ],
+                ),
+              child: SizedBox(
+                width: 400,
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    const Text(
+                      "Bienvenido",
+                      style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+              const SizedBox(height: 30),
+                  TextField(
+                  controller: emailController,
+                  decoration: InputDecoration(
+                    labelText: "Correo Electronico",
+                      border: OutlineInputBorder(
+                         borderRadius: BorderRadius.circular(12),
+                      ),
+                    ),
+                  ),
+                const SizedBox(height: 20),
+                    TextField(
+                    controller: passwordController,
+                    decoration: InputDecoration(
+                      labelText: "Contraseña",
+                        border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                    ),
+                      obscureText: true,
+                    ),
+                const SizedBox(height: 30),
+                  Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: <Widget>[
+                      ElevatedButton(
+                        onPressed: () => enviarDatos('login'),
+                        child: Text("Iniciar Sesion"),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ], //children
+      ),
+    );
+  }
+}
