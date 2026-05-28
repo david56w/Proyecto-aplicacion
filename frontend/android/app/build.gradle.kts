e5ac084c82dfb65f0deb6ac8831ce26b89a56c8d
david56w <david.martinez.alp@cbtis258.edu.mx>
2026-05-28 14:15:28 -0600

    iconos

--- frontend/android/app/build.gradle.kts
+++ frontend/android/app/build.gradle.kts
@@ -28,6 +28,7 @@ android {
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
+        manifestPlaceholders += mapOf("applicationLabel" to "Trvel X")
     }
 
     buildTypes {
