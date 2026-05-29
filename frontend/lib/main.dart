205f7eace130688511f14d1e1405712d56348a9f
david56w <david.martinez.alp@cbtis258.edu.mx>
2026-05-29 08:55:22 -0600

    fr

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -4,8 +4,8 @@ import 'view/login/widgets/splash_screen.dart';
 import 'view/login/login_page.dart';
 import 'view/login/register_page.dart';
 import 'view/dashboard/dashboard.dart';
-//import 'package:firebase_core/firebase_core.dart';
-//import 'services/notification_service.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'services/notification_service.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Supabase.initialize(
@@ -13,9 +13,9 @@ Future<void> main() async {
     anonKey: 'sb_publishable_W5ZT8SC93LNsBxqcIylKZw_QXkcdMt7',
   );
 
-  //await Firebase.initializeApp();
+  await Firebase.initializeApp();
 
- //await NotificationService.inicializarNotificaciones();
+  await NotificationService.inicializarNotificaciones();
 
   runApp(const MyApp());
 }
